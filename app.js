344f50bfd6be65bca5bb7369fdd4a05e6290cc9f
Yash Goswami <yashgoswami148@gmail.com>
2020-05-20 10:39:24 +0530

    done about and contact

--- app.js
+++ app.js
@@ -15,18 +15,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home",{startingContent:homeStartingContent});
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{aboutContent:aboutContent});
+});
 
+app.get("/Contact",function(req,res){
+  res.render("contact",{contactContent:contactContent});
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
